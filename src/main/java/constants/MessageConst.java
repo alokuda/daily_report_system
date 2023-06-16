c18e682ab08466822dc506820179b7865bafc216
m_okuda <m_okuda@MOUSE-175>
2023-06-16 14:44:02 +0900

    従業員destroyアクションの作成

--- src/main/java/constants/MessageConst.java
+++ src/main/java/constants/MessageConst.java
@@ -15,7 +15,7 @@ public enum MessageConst {
     // DB更新
     I_REGISTERED("登録が完了しました。"),
     I_UPDATE("更新が完了しました。"),
-    I_DERETED("削除が完了しました。"),
+    I_DELETED("削除が完了しました。"),
 
     // バリデーション
     E_NONAME("氏名を入力してください。"),
